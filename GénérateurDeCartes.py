53a9f02cc18adc80df2164500dbfb1d27ea25b3c
judi_Co <70235437+judiCo99@users.noreply.github.com>
2021-06-26 17:59:49 +0200

    Add files via upload

--- GénérateurDeCartes.py
+++ GénérateurDeCartes.py
@@ -1,6 +1,6 @@
 #by judi_Co
 __author__ = "judi_Co"
-Version = "Alpha"
+Version = 1
 
 import random
 
@@ -28,61 +28,64 @@ Liste0 = {"Jokers": "Jokers", "Jokers": "Jokers"}
 #==========================================
 #initialisation des liste de carte
 #cards liste init
-Piques = {}
-for cle, Valeur in Carte.items():
-	Piques[f"{cle} de Piques"] = Valeur
+def piocheCartedictionnaire(Carte=Carte):
+	Piques = {}
+	for cle, Valeur in Carte.items():
+		Piques[f"{cle} de Piques"] = Valeur
 
-Carreaux = {}
-for cle, Valeur in Carte.items():
-	Carreaux[f"{cle} de Carreaux"] = Valeur
+	Carreaux = {}
+	for cle, Valeur in Carte.items():
+		Carreaux[f"{cle} de Carreaux"] = Valeur
 
-Coeurs = {}
-for cle, Valeur in Carte.items():
-	Coeurs[f"{cle} de Coeurs"] = Valeur
+	Coeurs = {}
+	for cle, Valeur in Carte.items():
+		Coeurs[f"{cle} de Coeurs"] = Valeur
 
-Tréfles = {}
-for cle, Valeur in Carte.items():
-	Tréfles[f"{cle} de Tréfles"] = Valeur
+	Tréfles = {}
+	for cle, Valeur in Carte.items():
+		Tréfles[f"{cle} de Tréfles"] = Valeur
 
 
-#==========================================
-#Pile de cartes
-#Stack of cards
-Pile = {}
+	#==========================================
+	#Pile de cartes
+	#Stack of cards
+	Pile = {}
 
-Pile.update(Piques)
-Pile.update(Carreaux)
-Pile.update(Coeurs)
-Pile.update(Tréfles)
+	Pile.update(Piques)
+	Pile.update(Carreaux)
+	Pile.update(Coeurs)
+	Pile.update(Tréfles)
 
-#==========================================
-#mélanger la pile de carte
-#shuffle cards
-Keys = list(Pile.keys())
-random.shuffle(Keys)
-pioche = {}
-for key in Keys:
-	pioche.update({key:Pile[key]})
+	#==========================================
+	#mélanger la pile de carte
+	#shuffle cards
+	Keys = list(Pile.keys())
+	random.shuffle(Keys)
+	pioche = {}
+	for key in Keys:
+		pioche.update({key:Pile[key]})
+	return pioche
 
 #==========================================
 #afficher les carte
 #cards display
-def printCarte():
+def printCarteListe(pioche=piocheCartedictionnaire()):
 	for cle in pioche.keys():
 		Cles = list(pioche)
-		print(cle)
 	return Cles
+
 #==========================================
 #afficher les cartes
 #cards display
-Liste = printCarte()
+#Liste = printCarteListe()
+#Liste = piocheCartedictionnaire()
 
 #afficher les cartes sous forme de Liste
 #display maps in List form
-print(Liste)
+print(printCarteListe())
 
 #afficher les cartes sous forme de dictionnaire
 #display maps in dictionary form
-print(pioche)
+print(piocheCartedictionnaire())
 
 #==========================================
