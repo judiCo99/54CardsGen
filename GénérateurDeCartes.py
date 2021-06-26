f0e2ce3c2652aec8d2be50a64033d3c2332130e3
judiCo99 <70235437+judiCo99@users.noreply.github.com>
2021-06-26 05:26:35 +0200

    Add files via upload

---
+++ GénérateurDeCartes.py
@@ -0,0 +1,88 @@
+#by judi_Co
+__author__ = "judi_Co"
+Version = "Alpha"
+
+import random
+
+#==========================================
+#dictionnaire des carte
+#cards dictionary
+Carte = {
+	"As": ["As", 14], 
+	"2": ["2", 2], 
+	"3": ["3", 3], 
+	"4": ["4", 4], 
+	"5": ["5", 5], 
+	"6": ["6", 6], 
+	"7": ["7", 7], 
+	"8": ["8", 8], 
+	"9": ["9", 9], 
+	"10": ["10", 10], 
+	"Valet": ["Valet", 11], 
+	"Dame": ["Dame", 12], 
+	"Roi": ["Roi", 13]
+}
+
+Liste0 = {"Jokers": "Jokers", "Jokers": "Jokers"}
+
+#==========================================
+#initialisation des liste de carte
+#cards liste init
+Piques = {}
+for cle, Valeur in Carte.items():
+	Piques[f"{cle} de Piques"] = Valeur
+
+Carreaux = {}
+for cle, Valeur in Carte.items():
+	Carreaux[f"{cle} de Carreaux"] = Valeur
+
+Coeurs = {}
+for cle, Valeur in Carte.items():
+	Coeurs[f"{cle} de Coeurs"] = Valeur
+
+Tréfles = {}
+for cle, Valeur in Carte.items():
+	Tréfles[f"{cle} de Tréfles"] = Valeur
+
+
+#==========================================
+#Pile de cartes
+#Stack of cards
+Pile = {}
+
+Pile.update(Piques)
+Pile.update(Carreaux)
+Pile.update(Coeurs)
+Pile.update(Tréfles)
+
+#==========================================
+#mélanger la pile de carte
+#shuffle cards
+Keys = list(Pile.keys())
+random.shuffle(Keys)
+pioche = {}
+for key in Keys:
+	pioche.update({key:Pile[key]})
+
+#==========================================
+#afficher les carte
+#cards display
+def printCarte():
+	for cle in pioche.keys():
+		Cles = list(pioche)
+		print(cle)
+	return Cles
+#==========================================
+#afficher les cartes
+#cards display
+Liste = printCarte()
+
+#afficher les cartes sous forme de Liste
+#display maps in List form
+print(Liste)
+
+#afficher les cartes sous forme de dictionnaire
+#display maps in dictionary form
+print(pioche)
+
+#==========================================
